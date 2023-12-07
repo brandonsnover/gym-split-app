d5ff708657f503b6030c27702b347aac816d4cc5
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-12-07 12:49:57 -0600

    added exercise index, show, and create actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Exercise.create(name: "Deadlift", description: "full body primarily back", image_url: "deadlift url", video_url: "deadlift video")
+# Exercise.create(name: "Back Squat", description: "legs primaily quads", image_url: "Squat url", video_url: "Squat video")
+# Exercise.create(name: "Bench Press", description: "primarily chest with front delt and triceps", image_url: "Bench Press url", video_url: "Bench Press video")
