21bf8933a509e2c1d5b1032e11759baacfa73be5
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-12-07 12:57:52 -0600

    added update and destroy actions for exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -18,4 +18,21 @@ class ExercisesController < ApplicationController
     )
     render :show
   end
+
+  def update
+    @exercise = Exercise.find_by(id: params[:id])
+    @exercise.update(
+      name: params[:name] || @exercise.name,
+      description: params[:description] || @exercise.description,
+      image_url: params[:image_url] || @exercise.image_url,
+      video_url: params[:video_url] || @exercise.video_url,
+    )
+    render :show
+  end
+
+  def destroy
+    @exercise = Exercise.find_by(id: params[:id])
+    @exercise.destroy
+    render json: { message: "exercise deleted" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/exercises" => "exercises#index"
   get "/exercises/:id" => "exercises#show"
   post "/exercises/:id" => "exercises#create"
+  patch "/exercises/:id" => "exercises#update"
+  delete "/exercises/:id" => "exercises#destroy"
 end
